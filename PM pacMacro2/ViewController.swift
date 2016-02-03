7c5eeb8dd4495cd20fd4de057caee29431bb8f22
Josh Wu <joshwu@me.com>
2016-02-03 10:36:50 -0800

    set up static overlay for testing purposes

--- PM pacMacro2/ViewController.swift
+++ PM pacMacro2/ViewController.swift
@@ -1,9 +1,12 @@
 import Mapbox
 
 class ViewController: UIViewController {
+    //Controller for the mapview, i haven't renamed this since it would break a bunch of other elements
     
     var mapView: MGLMapView!
     
+    //Information
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,6 +19,31 @@ class ViewController: UIViewController {
             longitude: -123.122352),
             zoomLevel: 15, animated: false)
         view.addSubview(mapView)
+        
+        //Drawing overlay
+        drawBoundingRect()
+    }
+    func drawBoundingRect(){
+        //Setup center location
+        let vanLat = 49.280915
+        let vanLon = -123.122352
+        //Setting up coordinates
+        var coords: [CLLocationCoordinate2D] = [
+            CLLocationCoordinate2DMake(vanLat, vanLon),
+            CLLocationCoordinate2DMake(vanLat, vanLon + 0.002),
+            CLLocationCoordinate2DMake(vanLat - 0.002, vanLon + 0.002),
+            CLLocationCoordinate2DMake(vanLat - 0.002, vanLon)
+        ]
+        //Building Overlay
+        let tmpOverlay = MGLPolygon(coordinates: &coords, count: UInt(coords.count))
+        //Adding the overlay to the map
+        mapView.addAnnotation(tmpOverlay)
+    }
+    func mapView(mapView: MGLMapView, alphaForShapeAnnotation annotation: MGLShape) -> CGFloat{
+        return 0.5
+    }
+    func mapView(mapView: MGLMapView, fillColorForPolygonAnnotation annotation: MGLPolygon) -> UIColor {
+        return UIColor.whiteColor()
     }
     
 }
