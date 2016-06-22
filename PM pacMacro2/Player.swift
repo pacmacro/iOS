113ba3a1a0fc357008756672ad9c78f4bc4c03cc
Josh Wu <joshwu@me.com>
2016-06-21 22:03:30 -0700

    add initial player class

---
+++ PM pacMacro2/Player.swift
@@ -0,0 +1,24 @@
+//
+//  Player.swift
+//  PM pacMacro2
+//
+//  Created by Josh Wu on 2016-06-19.
+//  Copyright © 2016 Josh Wu. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class player{
+    var playerID:   Int
+    var playerName: String
+    var playerType: String
+    var coordinates: CLLocationCoordinate2D
+    
+    init(playerID: Int, playerName: String, playerType: String, coordinates: CLLocationCoordinate2D){
+        self.playerID   = playerID
+        self.playerName = playerName
+        self.playerType = playerType
+        self.coordinates = coordinates
+    }
+}
