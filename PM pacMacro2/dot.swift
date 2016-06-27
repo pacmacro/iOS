d8807b1905aa16293bdc24264e2d999c73b82b01
Josh Wu <joshwu@me.com>
2016-06-26 17:01:05 -0700

    add dot class to represent dot/pellet objects

---
+++ PM pacMacro2/dot.swift
@@ -0,0 +1,35 @@
+//
+//  dot.swift
+//  PM pacMacro2
+//
+//  Created by Josh Wu on 2016-06-26.
+//  Copyright © 2016 Josh Wu. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+public class dot {
+    private var id : String
+    private var coordinates : CLLocationCoordinate2D
+    private var isCollectedBool : Bool
+    
+    init(id : String, coordinates : CLLocationCoordinate2D, isCollectedBool : Bool){
+        self.id = id
+        self.coordinates = coordinates
+        self.isCollectedBool = isCollectedBool
+    }
+    
+    public func getCoordinates() -> CLLocationCoordinate2D{
+        return coordinates
+    }
+    
+    public func getId() -> String{
+        return id
+    }
+    
+    public func isCollected() -> Bool{
+        return isCollectedBool
+    }
+    
+}
