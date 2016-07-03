0d588ba00440714a0486cb098f92657cf15722dd
Josh Wu <joshwu@me.com>
2016-07-03 15:34:52 -0700

    include internal logic class

---
+++ PM pacMacro2/PacmacroInternal.swift
@@ -0,0 +1,22 @@
+//
+//  PacMacroInternal.swift
+//  PM pacMacro2
+//
+//  Created by Josh Wu on 2016-06-19.
+//  Copyright © 2016 Josh Wu. All rights reserved.
+//
+
+import UIKit
+
+class PacMacroInternal {
+    
+    private var currentPlayer : playerType = playerType.viewer;
+    #
+    enum playerType{
+        
+        case viewer
+        case ghost
+        case pacman
+    }
+    
+}
