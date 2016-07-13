537106834a92c3bfa6cbc4d4aebc1d075ba2ce6a
Josh Wu <joshwu@me.com>
2016-07-12 18:24:40 -0700

    implement functions to get visible map entities

--- PM pacMacro2/Game.swift
+++ PM pacMacro2/Game.swift
@@ -10,13 +10,57 @@ import UIKit
 
 class Game {
     
-    private var currentPlayer : playerType = playerType.viewer;
+    var players : [Player]
+    private var dots : [Dot]
+    private var currentPlayerType : playerType
+    private var currentPacmanStatus : pacmanStatus
+    
+    
+    init(players: [Player], dots : [Dot], currentPlayerType : playerType){
+        self.players = players
+        self.dots = dots
+        self.currentPlayerType = currentPlayerType
+        self.currentPacmanStatus = pacmanStatus.normal
+    }
+    
+    func getVisiblePlayers() -> [Player]{
+        switch currentPlayerType {
+        case playerType.pacmanViewer:
+            // Player can see everyone
+            return players
+            
+        case playerType.ghostViewer:
+            if(currentPacmanStatus == pacmanStatus.redPellet || currentPacmanStatus == pacmanStatus.captured){
+                // Player can see everyone
+                return players
+            } else {
+                // Player can see ghosts
+                let playersSansPacman = players.filter({$0.playerType != "pacman"})
+                return playersSansPacman
+            }
+        default:
+            let emptyPlayers : [Player] = []
+            return emptyPlayers
+        }
+    }
+    
+    func getVisibleDots() -> [Dot]{
+        return dots.filter({!$0.isCollected()})
+    }
+    
     enum playerType{
         
-        case viewer
+        case ghostViewer
+        case pacmanViewer
         case ghost
         case pacman
     }
     
+    enum pacmanStatus{
+        
+        case normal
+        case captured
+        case redPellet
+    }
     
 }
